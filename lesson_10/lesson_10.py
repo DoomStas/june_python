1d874761e06908c905e5dfdf4a5e62fe4eae843f
DoomStas <dumchikovstas@gmail.com>
2024-07-31 23:37:08 +0200

    fix all lessons + add lesson 10

---
+++ lesson_10/lesson_10.py
@@ -0,0 +1,60 @@
+def read_file(file_path):
+    data = []
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            li = line.strip().split()
+            number = int(li[0])
+            name = ' '.join(li[1:-2])
+            amount = float(li[-2][:-1])
+            category = li[-1]
+            data.append((number, name, amount,category))
+    return data
+
+def sum_by_category(data):
+    category_sum = {}
+    for _, _, amount, category in data:
+        if category in category_sum:
+            category_sum[category] += amount
+        else:
+            category_sum[category] = amount
+    return category_sum
+
+def money_member(data):
+    member_sum = {}
+    for _, name, amount, _ in data:
+        if name in member_sum:
+            member_sum[name] += amount
+        else:
+            member_sum[name] = amount
+    return member_sum
+
+def member_buy(data, member_name):
+    purchases = 0
+    spent = 0.0
+
+    for i in data:
+        if i [1] == member_name:
+            purchases += 1
+            spent += i[2]
+    return purchases, spent
+
+file_path = 'hw_10_test.txt'
+data = read_file(file_path)
+
+all_category = sum_by_category(data)
+print('Total cost by category: ')
+for category, total in all_category.items():
+    print(f'{category}: {total:.2f} ')
+
+member_total = money_member(data)
+print('Costs of each family member: ')
+for member, total in member_total.items():
+    print(f'{member}: {total:.2f}')
+
+user_name = input('Enter the name: ').strip()
+purchases_mem, total_spent = member_buy(data, user_name)
+print(f'Number of purchases {user_name}: {purchases_mem}')
+print(f'Total amount spent {user_name}: {total_spent:.2f}')
+
+
--- lesson_9/lesson_9.py
+++ lesson_9/lesson_9.py
@@ -49,6 +49,70 @@ print(f'Largest palidrome is {palidrome} received from {num_1} * {num_2}')
 
 # Task 4
 
+from typing import List
+import string
+
+
+def text_to_numbers(text: str) -> List[int]:
+    for char in string.punctuation:
+        text = text.replace(char, ' ')
+    return list(map(int, text.split()))
+
+
+def is_arithmetic_progression(numbers: List[int]) -> bool:
+    if len(numbers) < 2:
+        return False
+
+    d = numbers[1] - numbers[0]
+    for i in range(2, len(numbers)):
+        if numbers[i] - numbers[i - 1] != d:
+            return False
+    return True
+
+
+def is_geometric_progression(numbers: List[int]) -> bool:
+    if len(numbers) < 2:
+        return False
+
+    q = numbers[1] // numbers[0]
+    for i in range(2, len(numbers)):
+        if numbers[i] // numbers[i - 1] != q:
+            return False
+    return True
+
+
+def next_arihmetic_number(numbers: List[int]) -> int:
+    d = numbers[1] - numbers[0]
+    return numbers[-1] + d
+
+
+def next_geometric_number(numbers: List[int]) -> int:
+    q = numbers[1] // numbers[0]
+    return numbers[-1] * q
+
+
+def base(numbers: List[int]) -> int:
+    if is_arithmetic_progression(numbers):
+        return next_arihmetic_number(numbers)
+
+    if is_geometric_progression(numbers):
+        return next_geometric_number(numbers)
+
+    return None
+
+
+def main():
+    text = input("Enter a list of numbers separated by a space: ")
+    numbers = text_to_numbers(text)
+    res = base(numbers)
+    if res:
+        print(f"The next number in the sequence is: {res}")
+    else:
+        print(f"The sequence is not defined by an arithmetic or geometric progression.")
+
+
+if __name__ == "__main__":
+    main()
 
 
 # Task 5
@@ -128,3 +192,89 @@ def add(*args, **kwargs):
     return total
 
 print(add(1, 2, 3, 4, 5, 6, 7, a=1, b=2,c=3))
+
+# Task 1
+def salary(amount: int | float, tax_rate: int | float) -> int | float:
+    """
+    Returns the salary after tax deduction.
+    :param amount: The amount of money
+    :param tax_rate: The tax rate
+    :return: The salary after tax deduction
+    """
+    if not isinstance(amount, int | float):
+        raise TypeError('amount must be an int or float')
+    if not isinstance(tax_rate, int | float):
+        raise TypeError('tax_rate must be an int or float')
+    if amount < 0:
+        raise ValueError('amount must be a positive number')
+    if tax_rate < 0 or tax_rate > 1:
+        raise ValueError('tax_rate must be between 0 and 1')
+
+    return amount - amount * tax_rate
+
+
+# print(salary(1000, 0.1))
+# print(salary(1000, 1.2))
+# print(salary(1000, '0.5'))
+
+
+# Task 2
+def password_gen(length: int, special: bool = False) -> str:
+    """
+    Returns a password of the given length.
+    :param length: The length of the password
+    :param special: Whether to include special characters
+    :return: The generated password
+    """
+    import random
+    import string
+
+    if not isinstance(length, int):
+        raise TypeError('length must be an int')
+    if length < 2:
+        raise ValueError('length must be at least 2')
+
+    chars = string.ascii_letters + string.digits + string.punctuation if special else ''
+    return ''.join(random.sample(chars, length))
+
+
+# Task 3
+def is_palindrome(s: str) -> bool:
+    return s == s[::-1]
+
+
+def max_palindrom(start=100, stop=1000):
+    res = []
+    for i in range(start, stop):
+        for j in range(i, stop):
+            if is_palindrome(str(i * j)):
+                res.append((i * j, i, j))
+
+    return max(res, key=lambda tpl: tpl[0])
+
+
+res = max_palindrom()
+print(res)
+
+#Bonus task
+
+from typing import List
+
+def is_odd(item):
+    return item % 2
+
+def is_neg(item):
+    return item < 0
+
+def filter(x: List[int], func):
+    res = []
+    for item in x:
+        if func(item):
+            res.append(item)
+    return res
+
+x = [1, -2, 3, -4, 5, -6, 7, 8]
+print(filter(x, is_odd))
+print(filter(x, is_neg))
+print(filter(x, lambda item: item % 2 == 0))
+print(filter(x, lambda item: item > 0))
